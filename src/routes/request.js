26bf9b28113d96d0d40fae613bc60c2a733ea169
Balakrishna <balakrishnasatrapu@gmail.com>
2026-04-15 14:11:21 +0530

    feat: add validations to send connection request api

--- src/routes/request.js
+++ src/routes/request.js
@@ -2,21 +2,49 @@ const express = require("express");
 const requestRouter = express.Router();
 const userAuth = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
 requestRouter.post("/send/:status/:userId", userAuth, async (req, res) => {
     try {
         const loggedInUser = req.user;
+        const senderUserId = loggedInUser._id;
         const receiverUserId = req.params.userId;
         const status = req.params.status;
+
+        if(status !== "interested" && status !== "ignored") {
+            throw new Error("status not valid");
+        }
+
+        if(senderUserId === receiverUserId) {
+            throw new Error("invalid request");
+        }
+
+        const user = await User.findById(receiverUserId);
+
+        if(!user) {
+            throw new Error("user not found");
+        }
+
+        const existingRequest = await ConnectionRequest.findOne({
+            $or: [
+                {senderUserId, receiverUserId},
+                {senderUserId: receiverUserId, receiverUserId: senderUserId}
+            ]
+        });
+
+        if(existingRequest) {
+            throw new Error("connection already exists");
+        }
+
         const connection = new ConnectionRequest({
             senderUserId: loggedInUser._id,
-            receiverUserId: receiverUserId,
-            status: status
+            receiverUserId,
+            status
         });
 
         await connection.save();
 
-        res.send("request sent successful!!!");
+        res.send("request sent successfully!!!");
     } catch (err) {
         res.status(400).send("ERROR: " + err.message);
     }
